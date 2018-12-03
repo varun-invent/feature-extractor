5e1ef4d67e5a2c9bc3138e0b0184ea55efc4f52b
Varun Kumar <varun.invent@gmail.com>
2018-12-04 00:59:42 +0530

    Initial commit code working.

---
+++ merge_csv.py
@@ -0,0 +1,58 @@
+import glob
+import pandas as pd
+import numpy as np
+
+def merge_csv(path_regex, out_file='combined.csv'):
+    '''
+    Merges various CSV files and append new column of ROI Index and Preproc type
+    '''
+
+    file_list = []
+
+    combined_df = pd.DataFrame()
+
+    for filename in glob.iglob(path_regex, recursive=True):
+        opt = filename.split('/')[-1].split('_')[-1].split('.')[0]
+        print(opt)
+        df = pd.read_csv(filename)
+        # Assigning Options column
+        df = df.assign(option=pd.Series([opt]*len(df)))
+
+        # Assigning ROI index column
+        df = df.assign(ROI_idx=pd.Series(np.arange(1,275)))
+
+        combined_df = combined_df.append(df, ignore_index=True)
+
+
+        # import pdb; pdb.set_trace()
+    combined_df.to_csv(out_file,index= False)
+
+
+if __name__ == "__main__":
+    path_regex = "/home/varun/Projects/fmri/feature_extractor/data/**/*.csv"
+    out_file='combined1.csv'
+    merge_csv(path_regex)
+
+
+
+# folder_path = "/home/varun/Projects/fmri/feature_extractor/"
+#
+# file_list = []
+#
+# combined_df = pd.DataFrame()
+#
+# for filename in glob.iglob(folder_path + 'data/**/*.csv', recursive=True):
+#     opt = filename.split('/')[-1].split('_')[-1].split('.')[0]
+#     print(opt)
+#     df = pd.read_csv(filename)
+#     # Assigning Options column
+#     df = df.assign(option=pd.Series([opt]*len(df)))
+#
+#     # Assigning ROI index column
+#     df = df.assign(ROI_idx=pd.Series(np.arange(1,275)))
+#
+#     combined_df = combined_df.append(df, ignore_index=True)
+#
+#
+#     # import pdb; pdb.set_trace()
+# combined_df.to_csv('combined.csv',index= False)
