c568bd9e666c1d65acdc907b9a7769a9ce4c91e0
Varun Kumar <varun.invent@gmail.com>
2018-12-16 14:54:12 +0530

    Multiprocessing included for multiple UC OC thresholds

--- link_mapping_to_atlas/map_links_to_atlas.py
+++ link_mapping_to_atlas/map_links_to_atlas.py
@@ -12,6 +12,9 @@ import numpy as np
 import pandas as pd
 import nibabel as nib
 import xml.etree.ElementTree as ET
+from multiprocessing import Pool
+import multiprocessing.managers
+
 
 
 '''
@@ -23,6 +26,50 @@ Calculate number of UC and OC links
 For each Brain loop over the volumes and generate the CSV report for each of the
 volume
 '''
+def map_links_to_atlas(input_list):
+    atlas = 'fb'
+
+    thresh_UC,  thresh_OC = input_list[0]
+    UC_brain_path, OC_brain_path, out_file_prefix = input_list[1]
+    print('Working with UC %s and OC %s'%(thresh_UC, thresh_OC))
+
+    _volumes = 274
+    volumes = nib.load(UC_brain_path).shape[-1]
+    if _volumes != volumes:
+        raise Exception('Volumes not 274')
+
+
+    threshold = thresh_UC
+    contrast = UC_brain_path
+    obj = report(contrast, atlas, threshold)
+
+    combined_df = pd.DataFrame()
+
+    for volume in range(volumes):
+        path, df = obj.report(volume=volume)
+        if not df.empty:
+            df = df.assign(ROI_Idx=pd.Series([volume+1]*df.shape[0]))
+            df = df.assign(ROI_name=pd.Series([atlasDict[volume+1]]*df.shape[0]))
+            df = df.assign(Conectivity=pd.Series([-1]*df.shape[0]))
+            combined_df = combined_df.append(df, ignore_index=True)
+
+    threshold = thresh_OC
+    contrast = OC_brain_path
+    obj = report(contrast, atlas, threshold)
+
+    # combined_df = pd.DataFrame()
+    for volume in range(volumes):
+        path, df = obj.report(volume=volume)
+        if not df.empty:
+            df = df.assign(ROI_Idx=pd.Series([volume+1]*df.shape[0]))
+            df = df.assign(ROI_name=pd.Series([atlasDict[volume+1]]*df.shape[0]))
+            df = df.assign(Conectivity=pd.Series([1]*df.shape[0]))
+            combined_df = combined_df.append(df, ignore_index=True)
+
+    out_file = out_file_prefix + '_UC%s_OC%s.csv'%(thresh_UC,thresh_OC)
+    combined_df.to_csv(out_file,index= False)
+    print('Done with UC %s and OC %s'%(thresh_UC, thresh_OC))
+
 
 
 def calc_links_count(brain, thresh):
@@ -55,6 +102,12 @@ def get_roi_Num_Name_dict(atlasLabelsPath, ATLAS_XML_ZERO_START_INDEX):
 
     return atlasDict
 
+# def main(UC_brain_path, OC_brain_path):
+#     atlasLabelsPath = crt_path + '/Full_brain_atlas_thr0-2mm/fullbrain_atlas.xml'
+#     ATLAS_XML_ZERO_START_INDEX = True
+#
+#     atlasDict = get_roi_Num_Name_dict(atlasLabelsPath, ATLAS_XML_ZERO_START_INDEX)
+
 
 if __name__ == '__main__':
     UC_brain_path = '../ABIDE1/combined_brain_UC_ABIDE1.nii.gz'
@@ -65,63 +118,84 @@ if __name__ == '__main__':
 
     atlasDict = get_roi_Num_Name_dict(atlasLabelsPath, ATLAS_XML_ZERO_START_INDEX)
 
-    thresh_UC = 10
-    thresh_OC = 10
+    # thresh_UC = 10
+    # thresh_OC = 10
+    #
+    # atlas = 'fb'
+    #
+    # print('Number of UC links common in at least %s preproc pipelines is %s '%\
+    #                     (thresh_UC, calc_links_count(UC_brain_path, thresh_UC)))
+    #
+    # print('Number of OC links common in at least %s preproc pipelines is %s '%\
+    #                     (thresh_OC, calc_links_count(OC_brain_path,thresh_OC)))
+    #
+    # '''
+    # Results:
+    # -------
+    # Number of UC links common in at least 10 preproc pipelines is 13756
+    # Number of OC links common in at least 6 preproc pipelines is 2552
+    #
+    # Number of OC links common in at least 10 preproc pipelines is 306
+    #
+    # Number of UC links common in at least 17 preproc pipelines is 1120
+    # Number of OC links common in at least 17 preproc pipelines is 0
+    #
+    #
+    #
+    # '''
 
-    atlas = 'fb'
+    out_file_prefix = 'ABIDE1/ABIDE1_links_atlas_map'
+    # out_file = map_links_to_atlas(thresh_UC, UC_brain_path, thresh_OC, OC_brain_path, out_file_prefix)
 
-    print('Number of UC links common in at least %s preproc pipelines is %s '%\
-                        (thresh_UC, calc_links_count(UC_brain_path, thresh_UC)))
+    thresh_UC  = np.arange(8,17,2)
+    thresh_OC  = np.arange(8,17,2)
+    thresh_UC_OC = list(zip(thresh_UC, thresh_OC))
+    fixed_args = [UC_brain_path, OC_brain_path, out_file_prefix]
 
-    print('Number of OC links common in at least %s preproc pipelines is %s '%\
-                        (thresh_OC, calc_links_count(OC_brain_path,thresh_OC)))
+    input_list = []
+    for tupl in thresh_UC_OC:
+        input_list.append((list(tupl), fixed_args))
 
-    '''
-    Results:
-    -------
-    Number of UC links common in at least 10 preproc pipelines is 13756
-    Number of OC links common in at least 6 preproc pipelines is 2552
+    # Create pool of 8 workers i.e. 8 processors will be utilized
+    pool = Pool(8)
 
-    Number of OC links common in at least 10 preproc pipelines is 306 
-
-    Number of UC links common in at least 17 preproc pipelines is 1120
-    Number of OC links common in at least 17 preproc pipelines is 0
-
+    # Execute the function - function_perform_action() with the inputs from input_list
+    pool.map(map_links_to_atlas, input_list)
 
 
-    '''
 
-    volumes = 274
 
-    threshold = thresh_UC
-    contrast = UC_brain_path
-    obj = report(contrast, atlas, threshold)
-
-    combined_df = pd.DataFrame()
-
-    for volume in range(volumes):
-        path, df = obj.report(volume=volume)
-        if not df.empty:
-            df = df.assign(ROI_Idx=pd.Series([volume+1]*df.shape[0]))
-            df = df.assign(ROI_name=pd.Series([atlasDict[volume+1]]*df.shape[0]))
-            df = df.assign(Conectivity=pd.Series([-1]*df.shape[0]))
-            combined_df = combined_df.append(df, ignore_index=True)
-
-    threshold = thresh_OC
-    contrast = OC_brain_path
-    obj = report(contrast, atlas, threshold)
-
+    # volumes = 274
+    #
+    # threshold = thresh_UC
+    # contrast = UC_brain_path
+    # obj = report(contrast, atlas, threshold)
+    #
     # combined_df = pd.DataFrame()
-    for volume in range(volumes):
-        path, df = obj.report(volume=volume)
-        if not df.empty:
-            df = df.assign(ROI_Idx=pd.Series([volume+1]*df.shape[0]))
-            df = df.assign(ROI_name=pd.Series([atlasDict[volume+1]]*df.shape[0]))
-            df = df.assign(Conectivity=pd.Series([1]*df.shape[0]))
-            combined_df = combined_df.append(df, ignore_index=True)
-
-    out_file = '../ABIDE1/UC_OC_ABIDE1_links_atlas_map10_10.csv'
-    combined_df.to_csv(out_file,index= False)
+    #
+    # for volume in range(volumes):
+    #     path, df = obj.report(volume=volume)
+    #     if not df.empty:
+    #         df = df.assign(ROI_Idx=pd.Series([volume+1]*df.shape[0]))
+    #         df = df.assign(ROI_name=pd.Series([atlasDict[volume+1]]*df.shape[0]))
+    #         df = df.assign(Conectivity=pd.Series([-1]*df.shape[0]))
+    #         combined_df = combined_df.append(df, ignore_index=True)
+    #
+    # threshold = thresh_OC
+    # contrast = OC_brain_path
+    # obj = report(contrast, atlas, threshold)
+    #
+    # # combined_df = pd.DataFrame()
+    # for volume in range(volumes):
+    #     path, df = obj.report(volume=volume)
+    #     if not df.empty:
+    #         df = df.assign(ROI_Idx=pd.Series([volume+1]*df.shape[0]))
+    #         df = df.assign(ROI_name=pd.Series([atlasDict[volume+1]]*df.shape[0]))
+    #         df = df.assign(Conectivity=pd.Series([1]*df.shape[0]))
+    #         combined_df = combined_df.append(df, ignore_index=True)
+    #
+    # out_file = '../ABIDE1/UC_OC_ABIDE1_links_atlas_mapUC%s_OC%s.csv'%(thresh_UC,thresh_OC)
+    # combined_df.to_csv(out_file,index= False)
 
     '''
     Results
